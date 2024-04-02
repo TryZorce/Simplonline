6fae4dbdc95d5898955b93d4c21886fbaf0cc09f
TryZorce <christopher.moron@gmail.com>
2024-04-02 12:23:12 +0200

    All Function Cours & Promo

--- backend/src/Controllers/CoursController.php
+++ backend/src/Controllers/CoursController.php
@@ -13,10 +13,55 @@ class CoursController
         $coursRepository = new CoursRepository();
         $cours = $coursRepository->getAll();
 
-        $jsonReponse = json_encode($cours);
+        $jsonResponse = json_encode($cours);
 
         header('Content-Type: application/json');
 
-        echo ($jsonReponse);
+        echo ($jsonResponse);
+    }
+
+    public function getCoursById($id)
+    {
+        $coursRepository = new CoursRepository();
+        $cours = $coursRepository->getById($id);
+
+        if ($cours) {
+            $jsonResponse = json_encode($cours);
+            header('Content-Type: application/json');
+            echo ($jsonResponse);
+        } else {
+            http_response_code(404);
+            echo "Cours not found";
+        }
+    }
+
+    public function createCours($jour, $periode, $idPromo)
+    {
+        $coursRepository = new CoursRepository();
+        $id = $coursRepository->create($jour, $periode, $idPromo);
+
+        if ($id) {
+            http_response_code(201);
+            echo "Cours created with ID: $id";
+        } else {
+            http_response_code(500);
+            echo "Failed to create cours";
+        }
+    }
+
+    public function updateCours($id, $jour, $periode, $idPromo)
+    {
+        $coursRepository = new CoursRepository();
+        $coursRepository->update($id, $jour, $periode, $idPromo);
+
+        echo "Cours updated successfully";
+    }
+
+    public function deleteCours($id)
+    {
+        $coursRepository = new CoursRepository();
+        $coursRepository->delete($id);
+
+        echo "Cours deleted successfully";
     }
 }
--- backend/src/Controllers/PromoController.php
+++ backend/src/Controllers/PromoController.php
@@ -13,10 +13,58 @@ class PromoController
         $promoRepository = new PromoRepository();
         $promos = $promoRepository->getAll();
 
-        $jsonReponse = json_encode($promos);
+        $jsonResponse = json_encode($promos);
 
         header('Content-Type: application/json');
 
-        echo ($jsonReponse);
+        echo ($jsonResponse);
+    }
+
+    public function getById($id)
+    {
+        $promoRepository = new PromoRepository();
+        $promo = $promoRepository->getById($id);
+
+        $jsonResponse = json_encode($promo);
+
+        header('Content-Type: application/json');
+
+        echo ($jsonResponse);
+    }
+
+    public function create($nom, $dateDebut, $dateFin, $places)
+    {
+        $promoRepository = new PromoRepository();
+        $id = $promoRepository->create($nom, $dateDebut, $dateFin, $places);
+
+        $jsonResponse = json_encode(['id' => $id]);
+
+        header('Content-Type: application/json');
+
+        echo ($jsonResponse);
+    }
+
+    public function update($id, $nom, $dateDebut, $dateFin, $places)
+    {
+        $promoRepository = new PromoRepository();
+        $promoRepository->update($id, $nom, $dateDebut, $dateFin, $places);
+
+        $jsonResponse = json_encode(['status' => 'success']);
+
+        header('Content-Type: application/json');
+
+        echo ($jsonResponse);
+    }
+
+    public function delete($id)
+    {
+        $promoRepository = new PromoRepository();
+        $promoRepository->delete($id);
+
+        $jsonResponse = json_encode(['status' => 'success']);
+
+        header('Content-Type: application/json');
+
+        echo ($jsonResponse);
     }
 }
