f13625d2b02403889dfdd5dc41732e62a1de3c73
TryZorce <christopher.moron@gmail.com>
2024-04-02 14:22:02 +0200

    add path api

--- backend/src/config.exemple.php
+++ backend/src/config.exemple.php
@@ -6,6 +6,8 @@ define("DATABASE_USERNAME", "VOTRE NOM");
 define("DATABASE_PASSWORD", "VOTRE MDP");
 
 define("BASE_API_URL", "METTRE VOTRE BASE API");
-define("API_STUDENT", BASE_API_URL . "/student");
+define("API_USERS", BASE_API_URL . "/users");
+define("API_COURS", BASE_API_URL . "/cours");
+define("API_PROMO", BASE_API_URL . "/promo");
 
 define("CORS_ALLOWED_ORIGINS", "*");
--- backend/src/router.php
+++ backend/src/router.php
@@ -1,6 +1,8 @@
 <?php
 
 $userController = new UsersController();
+$coursController = new CoursController();
+$promoController = new PromoController();
 
 $route = $_SERVER['REQUEST_URI'];
 $methode = $_SERVER['REQUEST_METHOD'];
@@ -9,14 +11,35 @@ switch ($route) {
     case API_USERS:
         switch ($methode) {
             case 'GET':
-                $userController->getAllUsers($id_users);
+                $userController->getAllUsers();
                 $userController->getUpdateUsers($name, $first_name, $activity, $password, $mail);
                 $userController->getDeleteUsers($id_users);
                 break;
             case 'POST':
                 $userController->getCreateUsers($name, $first_name, $activity, $password, $mail);
                 break;
-
+        }
+    case API_COURS:
+        switch ($methode) {
+            case 'GET':
+                $coursController->getAllCours();
+                $coursController->updateCours($id, $jour, $periode, $idPromo);
+                $coursController->deleteCours($id);
+                break;
+            case 'POST':
+                $coursController->createCours($jour, $periode, $idPromo);
+                break;
+        }
+    case API_PROMO:
+        switch ($methode) {
+            case 'GET':
+                $promoController->getAllPromo();
+                $promoController->updateCours($id, $nom, $dateDebut, $dateFin, $places);
+                $promoController->deleteCoursById($id);
+                break;
+            case 'POST':
+                $promoController->createCours($nom, $dateDebut, $dateFin, $places);
+                break;
         }
         break;
 }
