08a55538ea4c8d38e95faf374d6b6a2280cecd75
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-04-02 12:27:29 +0200

    change api users

--- backend/src/router.php
+++ backend/src/router.php
@@ -6,7 +6,7 @@ $route = $_SERVER['REQUEST_URI'];
 $methode = $_SERVER['REQUEST_METHOD'];
 
 switch ($route) {
-    case API_STUDENT:
+    case API_USERS:
         switch ($methode) {
             case 'GET':
                 $studentController->getAllUsers();
