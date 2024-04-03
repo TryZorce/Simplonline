6b7f0f51ca4ac58dcbe5feddc9fcad24b0f954b3
Joana Rodrigues <157580520+JoanaSolaki@users.noreply.github.com>
2024-04-03 11:56:44 +0200

    Init all html (#18)
    * Index en cours
    
    * Fix Cours
    
    * AJAX and POST connexion

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,9 @@
-const connexionUrl = "http://localhost:3000/backend/public";
+const url = "http://localhost:3000/backend/public";
+
 
 document
-  .getElementById("buttonExercice1")
-  .addEventListener("click", () => fetchGet(apiStudentsUrl));
+  .getElementById("connexion_email")
+  .addEventListener("click", () => fetchGet(url));
 
 function fetchGet(url) {
   fetch(url)
@@ -21,41 +22,48 @@ function fetchGet(url) {
 }
 
 // document
-//   .getElementById("signUpSubmitButton")
+//   .getElementById("connexion_email")
 //   .addEventListener("click", signUpRequest);
 
 // function signUpRequest(event) {
-//   event.preventDefault();
+//     event.preventDefault();
+  
+//     const name = document.getElementById("name").value;
+//     const password = document.getElementById("password").value;
+  
+//     const requestBody = {
+//         name: name,
+//         password: password,
+//       };
+    
+//       fetchPost(url, requestBody);
+//     }
 
-//   const name = document.getElementById("name").value;
-//   const password = document.getElementById("password").value;
+function fetchPost(url, body) {
+  fetch(url, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+  })
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error("Erreur de réseau : " + response.status);
+      }
+      return response.json();
+  })
+  .then((data) => {
+      console.log(data);
+  })
+  .catch((error) => {
+      console.error("Erreur lors de la requête :", error);
+  });
+}
 
-//   const requestBody = {
-//     name: name,
-//     password: password,
-//   };
+fetchPost('http://localhost:3000/backend/public', { key: 'value' });
 
-//   fetchPost(apiStudentsUrl, requestBody);
-// }
-
-// function fetchPost(url, body) {
-//   fetch(url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(body),
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Erreur de réseau : " + response.status);
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.error("Erreur lors de la requête :", error);
-//     });
-// }
+// Si tu veux l'utiliser sur un bouton au clic du formulaire par ex :
+document
+  .getElementById("connexion_email")
+  .addEventListener("click", () => fetchPost(url, { key: 'value' }));
