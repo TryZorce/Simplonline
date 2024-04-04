041c2c3528a7f7e812e40bc66e32414ee2bf977e
Jasmine DRAPERI <157580649+SubRayZero@users.noreply.github.com>
2024-04-04 11:51:42 +0200

    Jaja (#29)
    * login user
    
    * login users
    
    * login for users
    
    * login users
    
    * git push "merge front"
    
    * create user
    
    * update api url in router
    
    ---------
    
    Co-authored-by: Christopher Moron <68192471+TryZorce@users.noreply.github.com>

--- backend/src/Controllers/CoursController.php
+++ backend/src/Controllers/CoursController.php
@@ -7,12 +7,12 @@ class CoursController
 {
     use Response;
     use Cors;
-    
+
     public function getAllCours()
     {
         $coursRepository = new CoursRepository();
         $cours = $coursRepository->getAll();
-        
+
         header('Content-Type: application/json');
         $jsonResponse = json_encode($cours);
 
@@ -20,49 +20,48 @@ class CoursController
         echo ($jsonResponse);
     }
 
-    // public function getCoursById($id)
-    // {
-    //     $coursRepository = new CoursRepository();
-    //     $cours = $coursRepository->getById($id);
+    public function getCoursById($id)
+    {
+        $coursRepository = new CoursRepository();
+        $cours = $coursRepository->getById($id);
 
-    //     if ($cours) {
-    //         $jsonResponse = json_encode($cours);
-    //         header('Content-Type: application/json');
-    //         echo ($jsonResponse);
-    //     } else {
-    //         http_response_code(404);
-    //         echo "Cours not found";
-    //     }
-    // }
+        if ($cours) {
+            $jsonResponse = json_encode($cours);
+            header('Content-Type: application/json');
+            echo ($jsonResponse);
+        } else {
+            http_response_code(404);
+            echo "Cours not found";
+        }
+    }
 
-    // public function createCours($jour, $periode, $idPromo)
-    // {
-    //     $coursRepository = new CoursRepository();
-    //     $id = $coursRepository->create($jour, $periode, $idPromo);
+    public function createCours($jour, $periode, $idPromo)
+    {
+        $coursRepository = new CoursRepository();
+        $id = $coursRepository->create($jour, $periode, $idPromo);
 
-    //     if ($id) {
-    //         http_response_code(201);
-    //         echo "Cours created with ID: $id";
-    //     } else {
-    //         http_response_code(500);
-    //         echo "Failed to create cours";
-    //     }
-    // }
+        if ($id) {
+            http_response_code(201);
+            echo "Cours created with ID: $id";
+        } else {
+            http_response_code(500);
+            echo "Failed to create cours";
+        }
+    }
 
-    // public function updateCours($id, $jour, $periode, $idPromo)
-    // {
-    //     $coursRepository = new CoursRepository();
-    //     $coursRepository->update($id, $jour, $periode, $idPromo);
+    public function updateCours($id, $jour, $periode, $idPromo)
+    {
+        $coursRepository = new CoursRepository();
+        $coursRepository->update($id, $jour, $periode, $idPromo);
 
-    //     echo "Cours updated successfully";
-    // }
+        echo "Cours updated successfully";
+    }
 
-    // public function deleteCours($id)
-    // {
-    //     $coursRepository = new CoursRepository();
-    //     $coursRepository->delete($id);
+    public function deleteCours($id)
+    {
+        $coursRepository = new CoursRepository();
+        $coursRepository->delete($id);
 
-    //     echo "Cours deleted successfully";
-    // }
+        echo "Cours deleted successfully";
+    }
 }
-?>
