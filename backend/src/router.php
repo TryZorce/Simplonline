fd69d654ee43e461e13c97c4a71270f49e3862dd
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-04-05 09:31:33 +0200

    change the accolades

--- backend/src/router.php
+++ backend/src/router.php
@@ -18,172 +18,174 @@ switch ($route) {
                 $usersVerfiMail = $usersRepository->verifMail($mail);
                 break;
 
-        break;
+                break;
 
-    case API_USERS . '/update':
-        switch ($methode) {
-            case 'GET':
-                $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+            case API_USERS . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                    case 'POST':
+                        $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                }
+
                 break;
-            case 'POST':
-                $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+
+            case API_USERS . '/create':
+                switch ($methode) {
+                    case 'POST':
+                        $userController->getCreateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                }
+
                 break;
+
+            case API_USERS . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                    case 'POST':
+                        $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                }
+
+                break;
+
+            case API_USERS . '/create':
+                switch ($methode) {
+                    case 'POST':
+                        $userController->getCreateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
+                        break;
+                }
+
+                break;
+
+
+                break;
+
+            case API_COURS:
+                switch ($methode) {
+                    case 'GET':
+                        $coursController->getAllCours();
+                        break;
+                }
+
+                break;
+
+            case API_COURS . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $coursController->updateCours($id, $jour, $periode, $idPromo);
+                        break;
+                }
+
+                break;
+
+            case API_COURS . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $coursController->updateCours($id, $jour, $periode, $idPromo);
+                        break;
+                    case 'POST':
+                        $coursController->updateCours($id, $jour, $periode, $idPromo);
+                        break;
+                }
+
+                break;
+
+            case API_COURS . '/delete':
+                switch ($methode) {
+                    case 'GET':
+                        $coursController->deleteCours($id);
+                        break;
+                }
+
+                break;
+
+            case API_COURS . '/create':
+                switch ($methode) {
+                    case 'POST':
+                        $coursController->createCours($jour, $periode, $idPromo);
+                        break;
+                }
+
+                break;
+
+
+                break;
+
+            case API_PROMO:
+                switch ($methode) {
+                    case 'GET':
+                        $promoController->getAllPromo();
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/create':
+                switch ($methode) {
+                    case 'POST':
+                        $promoController->createPromo($nom, $dateDebut, $dateFin, $places);
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/create':
+                switch ($methode) {
+                    case 'POST':
+                        $promoController->createPromo($nom, $dateDebut, $dateFin, $places);
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/update':
+                switch ($methode) {
+                    case 'GET':
+                        $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
+                        break;
+
+
+                    case 'POST':
+                        $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/delete':
+                switch ($methode) {
+                    case 'GET':
+                        $promoController->deletePromoById($id);
+                        $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
+                        break;
+                }
+
+                break;
+
+            case API_PROMO . '/delete':
+                switch ($methode) {
+                    case 'GET':
+                        $promoController->deletePromoById($id);
+                        break;
+                }
+
+
+                break;
+            default:
+                $errorController->notFound();
         }
-
-        break;
-
-    case API_USERS . '/create':
-        switch ($methode) {
-            case 'POST':
-                $userController->getCreateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
-                break;}
-
-        break;
-
-    case API_USERS . '/update':
-        switch ($methode) {
-            case 'GET':
-                $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
-                break;
-            case 'POST':
-                $userController->getUpdateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
-                break;
-        }
-
-        break;
-
-    case API_USERS . '/create':
-        switch ($methode) {
-            case 'POST':
-                $userController->getCreateUsers($nom, $prénom, $activité, $mot_de_passe, $mail);
-                break;
-        }
-
-        break;
-
-
-        break;
-
-    case API_COURS:
-        switch ($methode) {
-            case 'GET':
-                $coursController->getAllCours();
-                break;
-        }
-
-        break;
-
-    case API_COURS . '/update':
-        switch ($methode) {
-            case 'GET':
-                $coursController->updateCours($id, $jour, $periode, $idPromo);
-                break;
-        }
-
-        break;
-
-    case API_COURS . '/update':
-        switch ($methode) {
-            case 'GET':
-                $coursController->updateCours($id, $jour, $periode, $idPromo);
-                break;
-            case 'POST':
-                $coursController->updateCours($id, $jour, $periode, $idPromo);
-                break;
-        }
-
-        break;
-
-    case API_COURS . '/delete':
-        switch ($methode) {
-            case 'GET':
-                $coursController->deleteCours($id);
-                break;
-        }
-
-        break;
-
-    case API_COURS . '/create':
-        switch ($methode) {
-            case 'POST':
-                $coursController->createCours($jour, $periode, $idPromo);
-                break;
-        }
-
-        break;
-
-
-        break;
-
-    case API_PROMO:
-        switch ($methode) {
-            case 'GET':
-                $promoController->getAllPromo();
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/create':
-        switch ($methode) {
-            case 'POST':
-                $promoController->createPromo($nom, $dateDebut, $dateFin, $places);
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/update':
-        switch ($methode) {
-            case 'GET':
-                $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/create':
-        switch ($methode) {
-            case 'POST':
-                $promoController->createPromo($nom, $dateDebut, $dateFin, $places);
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/update':
-        switch ($methode) {
-            case 'GET':
-                $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
-                break;
-
-
-            case 'POST':
-                $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/delete':
-        switch ($methode) {
-            case 'GET':
-                $promoController->deletePromoById($id);
-                $promoController->updatePromo($id, $nom, $dateDebut, $dateFin, $places);
-                break;
-        }
-
-        break;
-
-    case API_PROMO . '/delete':
-        switch ($methode) {
-            case 'GET':
-                $promoController->deletePromoById($id);
-                break;
-        }
-
-
-        break;
-    default:
-        $errorController->notFound();
 }
